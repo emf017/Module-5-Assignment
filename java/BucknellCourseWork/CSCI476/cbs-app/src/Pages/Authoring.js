76964d74762103e606be70630e66e1f6ca6ed6cc
Eva Frankovic <emf017@bucknell.edu>
2024-02-05 15:10:32 -0500

    NavBar

---
+++ java/BucknellCourseWork/CSCI476/cbs-app/src/Pages/Authoring.js
@@ -0,0 +1,11 @@
+import React from 'react'
+function Authoring(){
+    return (
+        <div className ="authoring">
+        <h1>  Authoring </h1>
+        </div>
+
+    )
+}
+ 
+export default Authoring;
